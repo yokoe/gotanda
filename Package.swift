f96fc9eeed4d5802f791f1743723127fa9e0ef87
Sota Yokoe <sota@kreuz45.com>
2020-02-16 18:49:22 +0900

    chore: Update package name to Gotanda.

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "gotanda",
+    name: "Gotanda",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "gotanda",
+            name: "Gotanda",
             targets: ["gotanda"]),
     ],
     dependencies: [
